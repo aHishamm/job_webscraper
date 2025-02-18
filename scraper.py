aad010139deec55f3c06836e518db939e5dfedfd
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2025-02-18 22:52:11 +0400

    Updated scraper.py imports

--- scraper.py
+++ scraper.py
@@ -1,3 +1,3 @@
 import pandas as pd 
 from jobspy import scrape_jobs # For Indeed and Linkedin 
-from utils import Bayt_jobs, NaukrigulfJobs # For Bayt and Naukrigulf
+from utils import Bayt_jobs, NaukrigulfJobs, LinkedinJobs, IndeedJobs # importing classes from utils.py
